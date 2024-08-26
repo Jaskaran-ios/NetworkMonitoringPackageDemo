dadb80d8a1f4b88672d852ea2921b8c1e353226b
jaskaran <jaskaran.singh@softradix.in>
2024-08-26 12:39:49 +0530

    update
    git commit -mupdate

--- Package.swift
+++ Package.swift
@@ -6,21 +6,19 @@ import PackageDescription
 let package = Package(
     name: "NetworkMonitoringPackageDemo",
     platforms: [
-          .iOS(.v12) // Specify the minimum iOS version your package supports
-      ],
+        .iOS(.v12)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "NetworkMonitoringPackageDemo",
             targets: ["NetworkMonitoringPackageDemo"]),
     ],
+    dependencies: [
+
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "NetworkMonitoringPackageDemo"),
-        .testTarget(
-            name: "NetworkMonitoringPackageDemoTests",
-            dependencies: ["NetworkMonitoringPackageDemo"]),
+            name: "NetworkMonitoringPackageDemo",
+            dependencies: []),
     ]
 )
